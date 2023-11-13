4b5723ee6425bb1d1ee208c3b841e5c05c94df16
AlbertoEE <albertolopezgarciaee@gmail.com>
2023-11-13 19:18:44 +0100

    Clearing warning.

--- app/character/creation/attributes/components/Description.tsx
+++ app/character/creation/attributes/components/Description.tsx
@@ -15,7 +15,7 @@ export default function Description(props: {
             <div>
                 <div className="bg-black w-full flex flex-row items-center p-0">
                     <Image className="m-0" src={`/imgs/${props.imgPath}`} alt="me" width="128" height="64" />
-                    <h1 className="font-orbitron text-2xl font-bold uppercase tracking-widest text-4xl bg-black p-4">{props.title}</h1>
+                    <h1 className="font-orbitron font-bold uppercase tracking-widest text-4xl bg-black p-4">{props.title}</h1>
                 </div>
             </div>
             <hr className="h-1 my-2 bg-gray-200 border-0 rounded" />
