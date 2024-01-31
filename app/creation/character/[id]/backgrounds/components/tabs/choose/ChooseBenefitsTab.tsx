79fad80d41ec73b85230545797121668db760798
alberto.garcia <alberto.garcia@personio.de>
2024-01-31 08:41:28 +0100

    No more repeated rows in Choose option.

--- app/creation/character/[id]/backgrounds/components/tabs/choose/ChooseBenefitsTab.tsx
+++ app/creation/character/[id]/backgrounds/components/tabs/choose/ChooseBenefitsTab.tsx
@@ -1,6 +1,4 @@
-import {
-  BackgroundDefinition
-} from "@/models/BackgroundDefinitionModels";
+import { BackgroundDefinition } from "@/models/BackgroundDefinitionModels";
 import { useStore } from "../../../state";
 import ChooseBenefitCell from "./ChooseBenefitCell";
 
@@ -12,19 +10,24 @@ export default function ChooseBenefitsTab(props: {
   return (
     <>
       <div className="p-5 whitespace-pre-line">
-        {"Here you can pick two other skills from the table below.\n\n You may pick the same skill twice if you wish, to improve its starting proficiency. No starting character can begin with a skill level higher than level-1."}
+        {
+          "Here you can pick two other skills from the table below.\n\n You may pick the same skill twice if you wish, to improve its starting proficiency. No starting character can begin with a skill level higher than level-1."
+        }
       </div>
       <div>{2 + "/2"}</div>
       <div className="flex flex-col">
-        {props.background.benefits.learning.map((benefit) => (
-          <div className="flex-1 py-1 px-3">
-            <ChooseBenefitCell benefit={benefit} />
-          </div>
-        ))}
+        {props.background.benefits.learning
+          .filter(
+            (obj, index, self) =>
+              index === self.findIndex((t) => t.name === obj.name)
+          )
+          .map((benefit) => (
+            <div className="flex-1 py-1 px-3">
+              <ChooseBenefitCell benefit={benefit} />
+            </div>
+          ))}
       </div>
-      <div>
-        {chosenBenefits.map(e => e.name)}
-      </div>
+      <div>{chosenBenefits.map((e) => e.name)}</div>
     </>
   );
 }
