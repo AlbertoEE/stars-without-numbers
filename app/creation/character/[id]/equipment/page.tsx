14deb5ad3e52b39ee3fe62e726b0bff7a638a1e0
rojuvi <rodrigo.juradovi@gmail.com>
2024-03-10 22:22:14 +0100

    Add equipment page

---
+++ app/creation/character/[id]/equipment/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import SplitDesign from "@/app/creation/components/SplitDesign";
+
+export default function EquipmentPage() {
+    return (
+        <SplitDesign
+            leftChild={<div>Equip</div>}
+            rightChild={<div>ment</div>}
+        />
+    )
+}
