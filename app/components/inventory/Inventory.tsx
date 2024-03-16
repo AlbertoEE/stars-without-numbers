840863d065a2366d5cc6edb6eaf461d6f1be974c
rojuvi <rodrigo.juradovi@gmail.com>
2024-03-16 16:29:51 +0100

    Switch inventory navbar with icons

--- app/components/inventory/Inventory.tsx
+++ app/components/inventory/Inventory.tsx
@@ -1,7 +1,13 @@
 import type Equipment from "@/models/equipment/EquipmentModels";
 import { type ReactElement } from "react";
 import ArmorItemCard from "./items/ArmorItemCard";
-import { Card, CardBody, CardFooter, CardHeader, Divider, Link, Navbar, NavbarContent, NavbarItem, ScrollShadow } from "@nextui-org/react";
+import { Card, CardBody, CardHeader, Divider, Link, Navbar, NavbarContent, NavbarItem, ScrollShadow } from "@nextui-org/react";
+import ARMOR_ICON from "@/public/imgs/equipment/armor.svg"
+import RANGED_WEAPONS_ICON from "@/public/imgs/equipment/ranged_weapon.svg"
+import MELEE_WEAPONS_ICON from "@/public/imgs/equipment/melee_weapon.svg"
+import HEAVY_WEAPONS_ICON from "@/public/imgs/equipment/heavy_weapon.svg"
+import GENERAL_EQUIPMENT from "@/public/imgs/equipment/general_equipment.svg"
+import { ITEM_ICON_DEFAULT_PROPS } from "./items/ItemCommons";
 
 export default function Inventory(props: {
     equipment: Equipment
@@ -11,38 +17,48 @@ export default function Inventory(props: {
         showingCategories.includes(category)
     return (
         <Card className="flex flex-col h-full w-full pb-5">
-            <Navbar isBordered className="flex-1 rounded-t-large border-small py-2" 
+            <Navbar isBordered className="flex-1 rounded-t-large border-small" 
                 classNames={{
                     item: [
+                        "flex",
+                        "flex-grow",
+                        "pb-1",
+                        "justify-center",
                         "data-[active=true]:after:content-['']",
                         "data-[active=true]:border-b-small"
                     ]
                 }}
             position="static">
-                <NavbarContent className="flex flex-wrap">
-                    <div className="flex justify-between w-full">
-                        <NavbarItem isActive={isShowingCategory("armor")}>
-                            <Link color="foreground" href="#armor">ARMOR</Link>
-                        </NavbarItem>
-                        <NavbarItem isActive={isShowingCategory("rangedWeapons")}>
-                            <Link color="foreground" href="#rangedWeapons">RANGED WEAPONS</Link>
-                        </NavbarItem>
-                        <NavbarItem isActive={isShowingCategory("meleeWeapons")}>
-                            <Link color="foreground" href="#meleeWeapons">MELEE WEAPONS</Link>
-                        </NavbarItem>
-                        <NavbarItem isActive={isShowingCategory("heavyWeapons")}>
-                            <Link color="foreground" href="#heavyWeapons">HEAVY WEAPONS</Link>
-                        </NavbarItem>
-                    </div>
-                    <div className="flex w-full justify-center">
-                        <NavbarItem isActive={isShowingCategory("generalEquipment")}>
-                            <Link color="foreground" href="#generalEquipment">GENERAL EQUIPMENT</Link>
-                        </NavbarItem>
-                    </div>
+                <NavbarContent className="flex justify-between w-full">
+                    <NavbarItem isActive={isShowingCategory("armor")}>
+                        <Link color="foreground" href="#armor">
+                            <ARMOR_ICON {...ITEM_ICON_DEFAULT_PROPS}/>
+                        </Link>
+                    </NavbarItem>
+                    <NavbarItem isActive={isShowingCategory("rangedWeapons")}>
+                        <Link color="foreground" href="#rangedWeapons">
+                            <RANGED_WEAPONS_ICON {...ITEM_ICON_DEFAULT_PROPS}/>
+                        </Link>
+                    </NavbarItem>
+                    <NavbarItem isActive={isShowingCategory("meleeWeapons")}>
+                        <Link color="foreground" href="#meleeWeapons">
+                            <MELEE_WEAPONS_ICON {...ITEM_ICON_DEFAULT_PROPS}/>
+                        </Link>
+                    </NavbarItem>
+                    <NavbarItem isActive={isShowingCategory("heavyWeapons")}>
+                        <Link color="foreground" href="#heavyWeapons">
+                            <HEAVY_WEAPONS_ICON {...ITEM_ICON_DEFAULT_PROPS}/>
+                        </Link>
+                    </NavbarItem>
+                    <NavbarItem isActive={isShowingCategory("generalEquipment")}>
+                        <Link color="foreground" href="#generalEquipment">
+                            <GENERAL_EQUIPMENT {...ITEM_ICON_DEFAULT_PROPS}/>
+                        </Link>
+                    </NavbarItem>
                 </NavbarContent>
             </Navbar>
             <ScrollShadow className="overflow-y-auto scroll-smooth p-5">
-                <Card radius="none" id="armor" className="mt-5 scroll-mt-5">
+                <Card radius="none" id="armor" className="mt-6 scroll-mt-6">
                     <CardHeader className="justify-center"><h1>ARMOR</h1></CardHeader>
                     <Divider />
                     <CardBody>
