f9b08cba5253b0ab14e471433cdbb899b06dc8b5
rojuvi <rodrigo.juradovi@gmail.com>
2024-03-17 12:20:08 +0100

    Implement inventory section highlight

--- app/components/inventory/Inventory.tsx
+++ app/components/inventory/Inventory.tsx
@@ -1,5 +1,5 @@
 import type Equipment from "@/models/equipment/EquipmentModels";
-import { type ReactElement } from "react";
+import { type ReactElement, useEffect, useState, useCallback } from "react";
 import ArmorItemCard from "./items/ArmorItemCard";
 import { Card, CardBody, CardHeader, Divider, Link, Navbar, NavbarContent, NavbarItem, ScrollShadow } from "@nextui-org/react";
 import ARMOR_ICON from "@/public/imgs/equipment/armor.svg"
@@ -12,12 +12,53 @@ import { ITEM_ICON_DEFAULT_PROPS } from "./items/ItemCommons";
 export default function Inventory(props: {
     equipment: Equipment
 }): ReactElement {
-    let showingCategories: string[] = ["armor"]
-    const isShowingCategory = (category: string): boolean =>
-        showingCategories.includes(category)
+    const [showingCategories, setShowingCategories] = useState([""])
+
+    const setShowingCategory = useCallback((category: string): void => {
+        const newShowingCategories = showingCategories.filter((v): boolean => v !== category)
+        newShowingCategories.push(category)
+        setShowingCategories(newShowingCategories)
+    }, [showingCategories, setShowingCategories])
+
+    const removeShowingCategory = useCallback((category: string): void => {
+        const newShowingCategories = showingCategories.filter((v): boolean => v !== category)
+        setShowingCategories(newShowingCategories)
+    }, [showingCategories, setShowingCategories])
+
+    useEffect((): () => void => {
+        // Get custom attribute item-category from all sections
+        const sections = document.querySelectorAll(".item-section");
+        const observers: Record<string, IntersectionObserver> = {}
+        sections.forEach((section): void => {
+            // create new instance and pass a callback function
+            const observer = new IntersectionObserver((entries): void => {
+                const visibleSection = entries.find((entry): boolean => entry.isIntersecting)?.target.id
+                // Update state with the visible section ID
+                if (visibleSection != null) {
+                    setShowingCategory(visibleSection)
+                } else {
+                    removeShowingCategory(section.id)
+                }
+            }, {
+                threshold: 0,
+                root: null,
+            });
+
+            observer.observe(section)
+
+            observers[section.id] = (observer)
+        })
+        // Cleanup function to remove observer
+        return (): void => {
+            sections.forEach((section): void => {
+                observers[section.id]?.unobserve(section)
+            })
+        }
+    }, [setShowingCategory, removeShowingCategory])
+
     return (
         <Card className="flex flex-col h-full w-full pb-5">
-            <Navbar isBordered className="flex-1 rounded-t-large border-small" 
+            <Navbar isBordered className="flex-1 rounded-t-large border-small"
                 classNames={{
                     item: [
                         "flex",
@@ -28,37 +69,37 @@ export default function Inventory(props: {
                         "data-[active=true]:border-b-small"
                     ]
                 }}
-            position="static">
+                position="static">
                 <NavbarContent className="flex justify-between w-full">
-                    <NavbarItem isActive={isShowingCategory("armor")}>
+                    <NavbarItem isActive={showingCategories.includes("armor")}>
                         <Link color="foreground" href="#armor">
-                            <ARMOR_ICON {...ITEM_ICON_DEFAULT_PROPS}/>
+                            <ARMOR_ICON {...ITEM_ICON_DEFAULT_PROPS} />
                         </Link>
                     </NavbarItem>
-                    <NavbarItem isActive={isShowingCategory("rangedWeapons")}>
+                    <NavbarItem isActive={showingCategories.includes("rangedWeapons")}>
                         <Link color="foreground" href="#rangedWeapons">
-                            <RANGED_WEAPONS_ICON {...ITEM_ICON_DEFAULT_PROPS}/>
+                            <RANGED_WEAPONS_ICON {...ITEM_ICON_DEFAULT_PROPS} />
                         </Link>
                     </NavbarItem>
-                    <NavbarItem isActive={isShowingCategory("meleeWeapons")}>
+                    <NavbarItem isActive={showingCategories.includes("meleeWeapons")}>
                         <Link color="foreground" href="#meleeWeapons">
-                            <MELEE_WEAPONS_ICON {...ITEM_ICON_DEFAULT_PROPS}/>
+                            <MELEE_WEAPONS_ICON {...ITEM_ICON_DEFAULT_PROPS} />
                         </Link>
                     </NavbarItem>
-                    <NavbarItem isActive={isShowingCategory("heavyWeapons")}>
+                    <NavbarItem isActive={showingCategories.includes("heavyWeapons")}>
                         <Link color="foreground" href="#heavyWeapons">
-                            <HEAVY_WEAPONS_ICON {...ITEM_ICON_DEFAULT_PROPS}/>
+                            <HEAVY_WEAPONS_ICON {...ITEM_ICON_DEFAULT_PROPS} />
                         </Link>
                     </NavbarItem>
-                    <NavbarItem isActive={isShowingCategory("generalEquipment")}>
+                    <NavbarItem isActive={showingCategories.includes("generalEquipment")}>
                         <Link color="foreground" href="#generalEquipment">
-                            <GENERAL_EQUIPMENT {...ITEM_ICON_DEFAULT_PROPS}/>
+                            <GENERAL_EQUIPMENT {...ITEM_ICON_DEFAULT_PROPS} />
                         </Link>
                     </NavbarItem>
                 </NavbarContent>
             </Navbar>
-            <ScrollShadow className="overflow-y-auto scroll-smooth p-5">
-                <Card radius="none" id="armor" className="mt-6 scroll-mt-6">
+            <ScrollShadow id="inventory-scroll" className="overflow-y-auto scroll-smooth px-5 tb-5">
+                <Card radius="none" id="armor" className="item-section mt-7 scroll-mt-7">
                     <CardHeader className="justify-center"><h1>ARMOR</h1></CardHeader>
                     <Divider />
                     <CardBody>
@@ -69,7 +110,7 @@ export default function Inventory(props: {
                         </div>
                     </CardBody>
                 </Card>
-                <Card radius="none" id="rangedWeapons" className="mt-6 scroll-mt-6">
+                <Card radius="none" id="rangedWeapons" className="item-section mt-7 scroll-mt-6">
                     <CardHeader className="justify-center"><h1>RANGED WEAPONS</h1></CardHeader>
                     <Divider />
                     <CardBody>
