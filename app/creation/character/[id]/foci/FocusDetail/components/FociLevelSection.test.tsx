657d109015b393ba629dbc15d2e470b63ac97c20
alberto.garcia <alberto.garcia@personio.de>
2024-03-31 11:40:20 +0200

    Rename tests

--- app/creation/character/[id]/foci/FocusDetail/components/FociLevelSection.test.tsx
+++ app/creation/character/[id]/foci/FocusDetail/components/FociLevelSection.test.tsx
@@ -1,34 +1,35 @@
 import { FocusType } from "@/models/FocusDefinitionModels"
-import { describe, expect, it } from "vitest"
+import { beforeEach, describe, expect, it } from "vitest"
 import { useStoreFociState, type FocusSelection } from "../../../state"
 import { render, screen } from "@testing-library/react"
 import { FociLevelSection } from "./FociLevelSection"
 
 describe("render button", (): void => {
   const initialStoreState = useStoreFociState.getState()
+  const focusedFocus = {
+    name: "assasin",
+    description: "",
+    levels: [
+      {
+        level: 0,
+        description: "",
+        descriptionSchema: ["", ""],
+        skillBenefitOptionList: ["survive"],
+      },
+      {
+        level: 1,
+        description: "",
+        descriptionSchema: ["", ""],
+      },
+    ],
+    type: FocusType.COMBAT_FOCUS,
+  }
 
   beforeEach((): any => {
     useStoreFociState.setState(initialStoreState, true)
   })
-  it("should return buy button, because 1 general points are available and no chosen foci", (): void => {
-    const focusedFocus = {
-      name: "assasin",
-      description: "",
-      levels: [
-        {
-          level: 0,
-          description: "",
-          descriptionSchema: ["", ""],
-          skillBenefitOptionList: ["survive"],
-        },
-        {
-          level: 1,
-          description: "",
-          descriptionSchema: ["", ""],
-        },
-      ],
-      type: FocusType.COMBAT_FOCUS,
-    }
+  it("should return buy button because 1 general points is available and there are no chosen foci", (): void => {
+    // Arrange
     const focusPoints = {
       generalFocusPoints: 1,
       combatFocusPoints: 0,
@@ -43,174 +44,112 @@ describe("render button", (): void => {
       chosenFoci,
     })
 
+    // Act
     render(<FociLevelSection focusedFocus={focusedFocus} level={wantedLevel} />)
 
+    // Assert
     expect(screen.getByText("Buy")).toBeDefined()
   })
-  // it("2", (): void => {
-  //   const focusDefinition = {
-  //     name: "assasin",
-  //     description: "",
-  //     levels: [
-  //       {
-  //         level: 0,
-  //         description: "",
-  //         descriptionSchema: ["", ""],
-  //         skillBenefitOptionList: ["survive"],
-  //       },
-  //       {
-  //         level: 1,
-  //         description: "",
-  //         descriptionSchema: ["", ""],
-  //       },
-  //     ],
-  //     type: FocusType.COMBAT_FOCUS,
-  //   }
-  //   const focusPoints = {
-  //     generalFocusPoints: 0,
-  //     combatFocusPoints: 1,
-  //     nonCombatFocusPoints: 0,
-  //     psychicFocusPoints: 0,
-  //   }
-  //   const chosenFoci: FocusSelection[] = [
-  //     {
-  //       focus: focusDefinition,
-  //       level: 1,
-  //       origin: "foci",
-  //     },
-  //   ]
-  //   const wantedLevel: number = 2
+  it("should return buy button for level 2 because 1 combat point is available and there is level 1 correct focus", (): void => {
+    // Arrange
+    const focusPoints = {
+      generalFocusPoints: 0,
+      combatFocusPoints: 1,
+      nonCombatFocusPoints: 0,
+      psychicFocusPoints: 0,
+    }
+    const chosenFoci: FocusSelection[] = [
+      {
+        focus: focusedFocus,
+        level: 1,
+        origin: "foci",
+      },
+    ]
+    const wantedLevel: number = 2
+    useStoreFociState.setState({
+      focusPoints,
+      chosenFoci,
+    })
 
-  //   const result = renderButton(
-  //     chosenFoci,
-  //     wantedLevel,
-  //     focusDefinition,
-  //     focusPoints,
-  //   )
+    // Act
+    render(<FociLevelSection focusedFocus={focusedFocus} level={wantedLevel} />)
 
-  //   expect(result).toBe("buy")
-  // })
-  // it("3", (): void => {
-  //   const focusDefinition = {
-  //     name: "assasin",
-  //     description: "",
-  //     levels: [
-  //       {
-  //         level: 0,
-  //         description: "",
-  //         descriptionSchema: ["", ""],
-  //         skillBenefitOptionList: ["survive"],
-  //       },
-  //       {
-  //         level: 1,
-  //         description: "",
-  //         descriptionSchema: ["", ""],
-  //       },
-  //     ],
-  //     type: FocusType.COMBAT_FOCUS,
-  //   }
-  //   const focusPoints = {
-  //     generalFocusPoints: 0,
-  //     combatFocusPoints: 1,
-  //     nonCombatFocusPoints: 0,
-  //     psychicFocusPoints: 0,
-  //   }
-  //   const chosenFoci: FocusSelection[] = [
-  //     {
-  //       focus: focusDefinition,
-  //       level: 1,
-  //       origin: "foci",
-  //     },
-  //   ]
-  //   const wantedLevel: number = 1
+    // Assert
+    expect(screen.getByText("Buy")).toBeDefined()
+  })
+  it("should return refund button because focus is already chosen", (): void => {
+    // Arrange
+    const focusPoints = {
+      generalFocusPoints: 0,
+      combatFocusPoints: 1,
+      nonCombatFocusPoints: 0,
+      psychicFocusPoints: 0,
+    }
+    const chosenFoci: FocusSelection[] = [
+      {
+        focus: focusedFocus,
+        level: 1,
+        origin: "foci",
+      },
+    ]
+    const wantedLevel: number = 1
+    useStoreFociState.setState({
+      focusPoints,
+      chosenFoci,
+    })
 
-  //   const result = renderButton(
-  //     chosenFoci,
-  //     wantedLevel,
-  //     focusDefinition,
-  //     focusPoints,
-  //   )
+    // Act
+    render(<FociLevelSection focusedFocus={focusedFocus} level={wantedLevel} />)
 
-  //   expect(result).toBe("refund")
-  // })
-  // it("4", (): void => {
-  //   const focusDefinition = {
-  //     name: "assasin",
-  //     description: "",
-  //     levels: [
-  //       {
-  //         level: 0,
-  //         description: "",
-  //         descriptionSchema: ["", ""],
-  //         skillBenefitOptionList: ["survive"],
-  //       },
-  //       {
-  //         level: 1,
-  //         description: "",
-  //         descriptionSchema: ["", ""],
-  //       },
-  //     ],
-  //     type: FocusType.COMBAT_FOCUS,
-  //   }
-  //   const focusPoints = {
-  //     generalFocusPoints: 0,
-  //     combatFocusPoints: 0,
-  //     nonCombatFocusPoints: 0,
-  //     psychicFocusPoints: 0,
-  //   }
-  //   const chosenFoci: FocusSelection[] = [
-  //     {
-  //       focus: focusDefinition,
-  //       level: 1,
-  //       origin: "foci",
-  //     },
-  //   ]
-  //   const wantedLevel: number = 2
+    // Assert
+    expect(screen.getByText("Refund")).toBeDefined()
+  })
+  it("should return blocked button because there are no available points", (): void => {
+    // Arrange
+    const focusPoints = {
+      generalFocusPoints: 0,
+      combatFocusPoints: 0,
+      nonCombatFocusPoints: 0,
+      psychicFocusPoints: 0,
+    }
+    const chosenFoci: FocusSelection[] = [
+      {
+        focus: focusedFocus,
+        level: 1,
+        origin: "foci",
+      },
+    ]
+    const wantedLevel: number = 2
+    useStoreFociState.setState({
+      focusPoints,
+      chosenFoci,
+    })
 
-  //   const result = renderButton(
-  //     chosenFoci,
-  //     wantedLevel,
-  //     focusDefinition,
-  //     focusPoints,
-  //   )
+    // Act
+    render(<FociLevelSection focusedFocus={focusedFocus} level={wantedLevel} />)
 
-  //   expect(result).toBe("blocked")
-  // })
-  // it("5", (): void => {
-  //   const focusDefinition = {
-  //     name: "assasin",
-  //     description: "",
-  //     levels: [
-  //       {
-  //         level: 0,
-  //         description: "",
-  //         descriptionSchema: ["", ""],
-  //         skillBenefitOptionList: ["survive"],
-  //       },
-  //       {
-  //         level: 1,
-  //         description: "",
-  //         descriptionSchema: ["", ""],
-  //       },
-  //     ],
-  //     type: FocusType.COMBAT_FOCUS,
-  //   }
-  //   const focusPoints = {
-  //     generalFocusPoints: 1,
-  //     combatFocusPoints: 1,
-  //     nonCombatFocusPoints: 0,
-  //     psychicFocusPoints: 0,
-  //   }
-  //   const chosenFoci: FocusSelection[] = []
-  //   const wantedLevel: number = 2
+    // Assert
+    expect(screen.getByText("Blocked")).toBeDefined()
+  })
+  it("should return blocked button because wanted focus is level 2 and no level 1 is already chosen", (): void => {
+    // Arrange
+    const focusPoints = {
+      generalFocusPoints: 1,
+      combatFocusPoints: 1,
+      nonCombatFocusPoints: 0,
+      psychicFocusPoints: 0,
+    }
+    const chosenFoci: FocusSelection[] = []
+    const wantedLevel: number = 2
+    useStoreFociState.setState({
+      focusPoints,
+      chosenFoci,
+    })
 
-  //   const result = renderButton(
-  //     chosenFoci,
-  //     wantedLevel,
-  //     focusDefinition,
-  //     focusPoints,
-  //   )
+    // Act
+    render(<FociLevelSection focusedFocus={focusedFocus} level={wantedLevel} />)
 
-  //   expect(result).toBe("blocked")
-  // })
+    // Assert
+    expect(screen.getByText("Blocked")).toBeDefined()
+  })
 })
