283448dd0ca9704748c15d3835cd621f8ab6712c
AlbertoEE <albertolopezgarciaee@gmail.com>
2024-03-31 17:01:36 +0200

    Test husky precommit

--- app/creation/character/[id]/foci/FocusDetail/FocusDetail.tsx
+++ app/creation/character/[id]/foci/FocusDetail/FocusDetail.tsx
@@ -9,7 +9,7 @@ export default function FocusDetail(): ReactElement {
   const { focusedFocus, focusPoints } = useStoreFociState()
 
   if (focusedFocus === undefined) return <></>
-
+//
   return (
     <div className="flex flex-col gap-4 w-full h-full py-5 px-5">
       <div className="flex flex-row items-center gap-4">
